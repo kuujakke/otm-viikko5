91d230a79bca42d18b24d94bb6ca7d51cf1136b4
Jakke Kuukkanen <jakke.kuukkanen@helsinki.fi>
2016-12-02 14:46:31 +0200

    Järjestelty asioita

--- maven-git-test/src/main/java/main.java
+++ maven-git-test/src/main/java/main.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by kuujakke on 2.12.2016.
  */
 public class main {
+    public static void main(String[] args) {
+        System.out.println("testing...");
+    }
 }
